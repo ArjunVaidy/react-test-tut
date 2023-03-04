910f58dfe2b9295d6447f6b10c0ed947d115e74e
Arjun071194 <arjun071194@yahoo.in>
2023-03-04 13:28:21 +0530

    Tdd code

---
+++ src/Secondtest/Tdd.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import Tdd from "./Tdd"
+
+test('test driven devlopment', () => {
+    render(<Tdd />);
+    const textEl = screen.getByText(/Hello/i);
+    expect(textEl).toBeInTheDocument();
+})
+
+
+test('test driven devlopment props', () => {
+    render(<Tdd name='Arjun'/>);
+    const textEl = screen.getByText(/Hello Arjun/i);
+    expect(textEl).toBeInTheDocument();
+})
