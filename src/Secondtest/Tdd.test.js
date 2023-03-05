3db7f0d5727bfb23bc96946229c82a5e57e702ee
Arjun071194 <arjun071194@yahoo.in>
2023-03-05 09:24:14 +0530

    descirbe method

--- src/Secondtest/Tdd.test.js
+++ src/Secondtest/Tdd.test.js
@@ -1,15 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import Tdd from "./Tdd"
 
-test('test driven devlopment', () => {
-    render(<Tdd />);
-    const textEl = screen.getByText(/Hello/i);
-    expect(textEl).toBeInTheDocument();
-})
-
-
-test.only('test driven devlopment props', () => {
-    render(<Tdd name='Arjun'/>);
-    const textEl = screen.getByText(/Hello Arjun/i);
-    expect(textEl).toBeInTheDocument();
+describe('Test Driven Development Group',() => {
+    test('test driven devlopment', () => {
+        render(<Tdd />);
+        const textEl = screen.getByText(/Hello/i);
+        expect(textEl).toBeInTheDocument();
+    })
+    
+    test('test driven devlopment props', () => {
+        render(<Tdd name='Arjun'/>);
+        const textEl = screen.getByText(/Hello Arjun/i);
+        expect(textEl).toBeInTheDocument();
+    })
 })
